630a997bea9acce43b73ba27d21dffbccb4d8590
Nandospto <nandospto@gmail.com>
2026-05-28 22:00:50 -0300

    Update das Pastas no git + .gitignore

--- backend/Models/ChatMessage.cs
+++
@@ -1,7 +0,0 @@
-namespace AssistentVirtualPrivada.Models;
-
-public class ChatMessage
-{
-    // O nome aqui deve bater com a chave do JSON que você enviará
-    public string Texto { get; set; } = string.Empty;
-}
